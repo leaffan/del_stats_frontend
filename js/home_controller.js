30da0df918ec4c502f700890150a13d65dbaf0e6
Markus Reinhold <leaffan@gmx.net>
2025-09-07 14:38:14 +0200

    Adjust home page for new season

--- js/home_controller.js
+++ js/home_controller.js
@@ -5,7 +5,7 @@ app.controller('homeController', function ($scope, $http, $cacheFactory) {
     // as parameter (which then would be called from the HTML)
     $http.get('data/2024/del_player_game_stats_aggregated.json', {cache: true}).then(function (res) {
         var team_players = [...new Set(res.data[1].map(item => item.team + '/' + item.player_id))];
-        $scope.random_team_player_2024 = team_players[Math.floor(Math.random() * team_players.length)];
+        $scope.random_team_player_2025 = team_players[Math.floor(Math.random() * team_players.length)];
     });
 
 });

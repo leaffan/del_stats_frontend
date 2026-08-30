b297fa2a5ba6dea89681abe599b6bb8fa0d6f026
Markus Reinhold <leaffan@gmx.net>
2026-08-31 00:29:12 +0200

    Set new default season in home controller

--- js/home_controller.js
+++ js/home_controller.js
@@ -1,11 +1,18 @@
-app.controller('homeController', function ($scope, $http, $cacheFactory, config) {
-
+app.controller('homeController', function ($scope, $http, $cacheFactory, config, svc) {
+    $scope.config = config;
+    $scope.svc = svc;
     // this is certainly not the way to do it but
     // I can't get this working as a function with season
     // as parameter (which then would be called from the HTML)
-    $http.get('data/' + config.defaultSeason + '/del_player_game_stats_aggregated.json', {cache: true}).then(function (res) {
-        var team_players = [...new Set(res.data[1].map(item => item.team + '/' + item.player_id))];
-        $scope.random_team_player_2025 = team_players[Math.floor(Math.random() * team_players.length)];
-    });
-
+    $http
+        .get('data/' + config.defaultSeason + '/del_player_game_stats_aggregated.json', {
+            cache: true,
+        })
+        .then(function (res) {
+            var team_players = [
+                ...new Set(res.data[1].map((item) => item.team + '/' + item.player_id)),
+            ];
+            $scope.random_team_player_default_season =
+                team_players[Math.floor(Math.random() * team_players.length)];
+        });
 });
